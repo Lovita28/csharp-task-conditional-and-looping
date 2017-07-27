3e40ce1aa25678e666878e66f0bff62e07ce4fae
Lovita28 <lovitaanjani@gmail.com>
2017-07-27 13:13:01 +0700

    csharp task conditional and looping

---
+++ Average/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter total Array :");
+            int n = int.Parse(Console.ReadLine());
+            int[] av = new int[n];
+
+            for (int x = 0; x < n; x++) {
+                Console.WriteLine("Input-{0} : ", x + 1);
+                av[x] = int.Parse(Console.ReadLine());
+            }
+
+            int total = 0;
+
+            for (int a = 0; a < n; a++) {
+                total = total + av[a];
+            }
+            double average = total / n;
+            Console.WriteLine("\nAverage is : {0}", average);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Bigsmall/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bigsmall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input 3 random numbers ;");
+            int[] bis = new int[3];
+            for (int x = 0; x < 3; x++) {
+                Console.Write("{0}. input :  ", x + 1);
+                bis[x] = int.Parse(Console.ReadLine());
+                
+            }
+            
+             
+            for (int a = 0; a < 3; a++) {
+                for (int b = 0; b < 3 - 1; b++) {
+                    if (bis[b + 1] > bis[b]) {
+                         int temp = bis[b];
+                        bis[b] = bis[b + 1];
+                        bis[b + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("The Biggest number : {0}", bis[0]);
+            Console.WriteLine("The Smallest number : {0}", bis[2]);
+            if (bis[0] > 0)
+            {
+                Console.WriteLine("the Biggest number is Positive");
+            }
+            else {
+                Console.WriteLine("The Biggest number is Negative");
+            }
+            if (bis[2] < 0)
+            {
+                Console.WriteLine("The Smallest number is Negative");
+            }
+            else {
+                Console.WriteLine("the Smallest number is Positive");
+            }
+
+           
+            Console.ReadKey();
+        }
+    }
+}
